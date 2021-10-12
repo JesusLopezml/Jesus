ec13260d96ca583803654b73676a60cfd49afee3
JesusLopezml <92120916+JesusLopezml@users.noreply.github.com>
2021-10-12 17:34:44 -0500

    Add files via upload

---
+++ Practica_array.cpp
@@ -0,0 +1,42 @@
+#include"iostream"
+using namespace std;
+int main(){
+	
+	//Imprimir un array o un arreglo con la palabra Jesus Antonio Lopez Miguel
+	char x[19];
+	     x[0]='J';
+	     x[1]='e';
+	     x[2]='s';
+	     x[3]='u';
+	     x[4]='s';
+	     x[5]=' ';
+	     x[6]='A';
+	     x[7]='n';
+	     x[8]='t';
+	     x[9]='o';
+	     x[10]='n';
+	     x[11]='i';
+	     x[12]='o';
+	     x[13]=' ';
+	     x[14]='L';
+	     x[15]='o';
+	     x[16]='p';
+	     x[17]='e';
+	     x[18]='z';
+	     x[19]=' ';
+	     x[20]='M';
+	     x[21]='i';
+	     x[22]='g';
+	     x[23]='u';
+	     x[24]='e';
+	     x[25]='l';
+	     for(int z=0; z<26; z++){//declarar el ciclo de las veces que se hara la impresion
+	     cout<<x[z];
+	     cout<<endl;
+	     	
+		 }
+
+	return 0;
+	cin.get();
+}
+
